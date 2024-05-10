24fdc2333d5e4aa23543414939fa68394d79a4ae
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-10 11:59:22 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,44 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) <= 2:
+        list_to_remove_elements.remove (0)
+        return [list_to_remove_elements]
+    elif 2 < len(list_to_remove_elements) < 6:
+        list_to_remove_elements.remove (0)
+        list_to_remove_elements.remove (5)
+        return [list_to_remove_elements]
+    else:
+        list_to_remove_elements.remove (0)
+        list_to_remove_elements.remove (5)
+        list_to_remove_elements.remove (6)
+        return [list_to_remove_elements]
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.insert (0, "Pink")
+    list_to_add_elements.insert (-1, "Yellow")
+    return [list_to_add_elements]
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check) == 0:
+        return True
+    else:
+        return False
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1) and len(list_to_compare2) > 3 and list_to_compare1 [2] == list_to_compare2 [2]:
+        return True
+    else:
+        return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_of_lists_to_modify [0][0]
+    list_of_lists_to_modify [0][1]
+    list_of_lists_to_modify [1][1]
+    list_of_lists_to_modify [1][2]
+    list_of_lists_to_modify [1][3]
+    list_of_lists_to_modify [2][-1]
+    list_of_lists_to_modify [2][-1]
+    return list_of_lists_to_modify
