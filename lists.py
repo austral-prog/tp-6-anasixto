1de217ecb269a7bbe909730a953114726c72ddec
anasixto <162721927+anasixto@users.noreply.github.com>
2024-05-10 12:45:38 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,11 +1,5 @@
 def remove_elements(list_to_remove_elements):
-    if len(list_to_remove_elements) > 0:
-        del list_to_remove_elements [0]
-    if len(list_to_remove_elements) > 4:
-        del list_to_remove_elements [3]
-    if len(list_to_remove_elements) > 5:
-        del list_to_remove_elements [4]
-    return list_to_remove_elements
+    
 
 
 def add_elements(list_to_add_elements):
@@ -22,18 +16,15 @@ def is_empty(list_to_check):
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    if len(list_to_compare1) > 3 and len(list_to_compare2) > 3 and list_to_compare1 [2] == list_to_compare2 [2]:
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3 and list_to_compare1 [2] == list_to_compare2 [2]:
         return True
     else:
         return False
 
 
 def list_of_lists(list_of_lists_to_modify):
-    del list_of_lists_to_modify [0][0]
-    del list_of_lists_to_modify [0][1]
-    del list_of_lists_to_modify [1][1]
-    del list_of_lists_to_modify [1][2]
-    del list_of_lists_to_modify [1][3]
-    del list_of_lists_to_modify [2][-1]
-    del list_of_lists_to_modify [2][-2]
+    
+    list_of_lists_to_modify[0] = list_of_lists_to_modify[0][0:2]
+    list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
+    list_of_lists_to_modify[2] = list_of_lists_to_modify[2][-2::]
     return list_of_lists_to_modify
